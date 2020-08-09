ae28f9ea1978df92c049f54f41caf52a5712e182
Leonardo Borges Neres <leonardo.borges.neres@gmail.com>
2020-08-09 18:28:50 -0300

    Mobile
    Api

--- frontend/src/Pages/TeacherForm/index.tsx
+++ frontend/src/Pages/TeacherForm/index.tsx
@@ -69,7 +69,7 @@ const TeacherForm: React.FC = () => {
     <div id="page-teacher-form" className="container">
       <PageHeader 
         title="Que incrível que você quer dar aulas." 
-        description="O primeiro passo, é preencher esse formulário de inscrição."
+
       />
 
       <main>
